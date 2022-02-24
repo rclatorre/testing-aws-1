c9cfd43a3862799c94b303abebf7818133629483
Roberto Latorre <macbook@Macbooks-MacBook-Pro.local>
2022-02-23 22:06:20 -0500

    se modifica url del backend

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import React from 'react'
 function App() {
   const [respuesta, setRespuesta] = React.useState()
 
-  axios.get('http://localhost:4000/')
+  axios.get('http://api.wiracocha.click/')
   .then( resp => {
     const data = resp.data
     setRespuesta(data)
